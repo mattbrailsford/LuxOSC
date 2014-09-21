866f290b6dd9d8ce79169228845b2037bf47bfed
Matt Brailsford <me@mattbrailsford.com>
2014-09-21 17:54:31 +0000

    Fixed so things shutdown properly on Ctrl+C

--- luxosc.py
+++ luxosc.py
@@ -1,63 +1,66 @@
-from OSC import OSCServer,OSCClient, OSCMessage
-import OSC
+# Imports
 import sys
-from time import sleep
 import time
 import types
-import os
-import RPi.GPIO as GPIO
+import threading
+import OSC
+
+from time import sleep
+from OSC import OSCServer,OSCClient, OSCMessage
 from Adafruit_PWM_Servo_Driver import PWM
 
-runState = True
+# Welcome
+print r" ___       ___  ___     ___    ___ ________  ________  ________      "
+print r"|\  \     |\  \|\  \   |\  \  /  /|\   __  \|\   ____\|\   ____\     "
+print r"\ \  \    \ \  \\\  \  \ \  \/  / | \  \|\  \ \  \___|\ \  \___|     "
+print r" \ \  \    \ \  \\\  \  \ \    / / \ \  \\\  \ \_____  \ \  \        "
+print r"  \ \  \____\ \  \\\  \  /     \/   \ \  \\\  \|____|\  \ \  \____   "
+print r"   \ \_______\ \_______\/  /\   \    \ \_______\____\_\  \ \_______\ "
+print r"    \|_______|\|_______/__/ /\ __\    \|_______|\_________\|_______| "
+print r"                       |__|/ \|__|             \|_________|          "
+print r"                                                                     "                                                                    
 
-server = OSCServer( ("192.168.0.12", 8000) )#This has to be the IP of the RaspberryPi on the network
-client = OSCClient()
-pwm = PWM(0x40, debug=True)
+# Variables
+address = '192.168.0.12',8000
 
 neckMin = 140
 neckMax = 610
-
 tiltMin = 110
 tiltMax = 520
+panMin  = 60
+panMax  = 625
 
-panMin = 60
-panMax = 625
+server = OSCServer(address)
+#server.addDefaultHandlers()
 
+pwm = PWM(0x40, debug=True)
 pwm.setPWMFreq(60)
 
-#Helper methods
+# Helper methods
 def arduino_map(x, in_min, in_max, out_min, out_max):
      return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
-# FADERS
-#################################################################################################################################################
-def fader(path, tags, args, source): 
-	value = int(args[0])#Value is the variable that will transform the input from the faders into whole numbers(easier to deal with); it will also get the 'y' value of the XP pads
-	#mapped_value = arduino_map(value, 0, 180, servoMin, servoMax) 
-	print "Fader: ",value
-	#print "Mapped: ",mapped_value
+# Handlers
+def elbow(path, tags, args, source): 
+	value = int(args[0]) 
+	print "Elbow: ",value
 	pwm.setPWM(0, 0, arduino_map(value, 0, 100, neckMin, neckMax))
 
-# XY PADS
-###############################################################################################################################################
-def xypad(path, tags, args, source):
+def head(path, tags, args, source):
 	yy = int(args[0])
-	xx = int(args[1])#Value 2 is used with XP pads, it will get the 'x' value
-	print "XYPad: x=",xx,", y=",yy
+	xx = int(args[1])
+	print "Head: ",xx,",",yy
 	pwm.setPWM(1, 0, arduino_map(xx, 0, 100, panMin, panMax))
 	pwm.setPWM(2, 0, arduino_map(yy, 0, 100, tiltMin, tiltMax))
  
-# BUTTONS
-####################################################################################################################################################
-def button(path, tags, args, source):
+def btn(path, tags, args, source):
 	state=int(args[0])
-	print "Button: ",state
-	pwm.setPWM(0,0,0)
-	pwm.setPWM(1,0,0)
-	pwm.setPWM(2,0,0)
-	runState = False
+	print "Btn: ",state
+	if state == 1:
+		pwm.setPWM(0,0,0)
+		pwm.setPWM(1,0,0)
+		pwm.setPWM(2,0,0)
 
-# define a message-handler function for the server to call.
 def printer(addr, tags, stuff, source):
    	print "---"
     	print "received new osc msg from %s" % OSC.getUrlStr(source)
@@ -66,13 +69,28 @@ def printer(addr, tags, stuff, source):
     	print "data %s" % stuff
     	print "---"
 
-#These are all the add-ons that you can name in the TouchOSC layout designer (you can set the values and directories)
-server.addMsgHandler("/1/neck", fader)
-server.addMsgHandler("/1/head", xypad)
-server.addMsgHandler("/1/btn", button)
+# Hookup handlers
+server.addMsgHandler("/1/elbow", elbow)
+server.addMsgHandler("/1/head", head)
+server.addMsgHandler("/1/btn", btn)
 
-while runState:
-	print "Waiting..."
+# Main 
+print "\nRegistered Callback Functions:"
+for addr in server.getOSCAddressSpace():
+    print addr
 
-server.close()
-#This will kill the server when the program ends
+print "\nStarting OSCServer. Use Ctrl-C to quit."
+st = threading.Thread( target = server.serve_forever )
+st.start()
+
+try:
+	while 1:
+		#print "Waiting..."
+		sleep(5)
+
+except KeyboardInterrupt:
+	print "\nClosing OSCServer."
+	server.close()
+	print "Waiting for Server thread to finish."
+	st.join()
+	print "Done!"
